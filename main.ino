8f68d351e7b94ed008790b798f2fee1b173cf3a4
meredith <mmy.gladish@gmail.com>
2023-02-18 12:47:53 -0500

    Update main.ino

---
+++ main.ino
@@ -0,0 +1,20 @@
+#include "pitches.h"
+
+const int buttonNote = 2;
+const int buttonChord = 3;
+const int buttonReplay = 4;
+
+
+void setup() {
+    // put your setup code here, to run once:
+    pinMode(buttonNote, INPUT);
+    pinMode(buttonChord, INPUT);
+    pinmode(buttonReplay, IMPIT)
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    
+    // outpjut a note ?
+    
+}
